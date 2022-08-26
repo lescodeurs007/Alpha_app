e0e87aaaf0b5ef31d1089317d9347d3ee38cc8c6
lescodeurs007 <111956012+lescodeurs007@users.noreply.github.com>
2022-08-27 00:34:34 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -24,34 +24,23 @@ def select_items():
         #if vegetables
         if 'vegetables' in ch:
             st.write("VEGETABLES")
-            for file1 in os.listdir(base+'Vegtables'):
-                if file1.endswith(".jpg"):
-                    image1=Image.open(os.path.join(base+'Vegtables', file1))
-                    st.image(image1,width=100)
-                    f1=os.path.join(base+'Vegtables', file1)
-                    agree1= st.checkbox(f1[89:-4])
+            for j in ['beetroot', 'brinjal', 'brocoli rabi', 'capsicum', 'corn', 'cucumber', 'garlic', 'ginger', 'Image_4', 'lettuce', 'onions', 'peas', 'potato', 'pumkin', 'radish', 'soy beans', 'spinach', 'sweet potato', 'tomato', 'turnip']:
+                agree1= st.checkbox(j)
                 if agree1:
-                    vegetables.append(f1[89:-4])
+                    vegetables.append(j)
         #if snacks
         if 'snacks' in ch:
-            for file2 in os.listdir(base+'snacks'):
+            for k in ['cocount water', 'coffee', 'lemon juice', 'tea']:
                 if file2.endswith(".jpg"):
-                    image2=Image.open(os.path.join(base+'snacks', file2))
-                    st.image(image2,width=100)
-                    f2=os.path.join(base+'snacks', file2)
-                    agree2= st.checkbox(f2[86:-4])
+                    agree2= st.checkbox(k)
                 if agree2:
-                    snacks.append(f2[86:-4])
+                    snacks.append(k)
         #if juices
         if 'juices' in ch:
-            for file3 in os.listdir(base+'juices'):
-                if file3.endswith(".jpg"):
-                    image3=Image.open(os.path.join(base+'juices', file3))
-                    st.image(image3,width=100)
-                    f3=os.path.join(base+'juices', file3)
-                    agree3= st.checkbox(f3[86:-4])
+            for m in ['adhirasam', 'anarsa', 'bhel puri', 'dhai puri', 'kaalan', 'masala puri', 'pani puri', 'vada pav']:
+                agree3= st.checkbox(m)
                 if agree3:
-                    juices.append(f3[86:-4])
+                    juices.append(m)
 
     passing_value={"fruits":fruits,"vegetables":vegetables,"snacks":snacks,"juices":juices}
     print(passing_value)
